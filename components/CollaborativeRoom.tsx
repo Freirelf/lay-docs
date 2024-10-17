7187e1cb21590bbab85a52ef775a1e8ce188bae1
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-17 17:22:49 -0300

    Features - Liveblocks and Authenticate User with Real-time Features

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const CollaborativeRoom = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title"> Compartilhar </p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+        {children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
